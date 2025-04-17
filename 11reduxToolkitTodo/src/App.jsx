0ed625e87fb4bd82daa0f95689ae36f21244aaea
parth0512 <roney.bob11patel@gmail.com>
2025-04-17 10:11:56 +0530

    reduxtoolkit

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddTodo from "./Components/AddTodo";
+import Todos from "./Components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Lear about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
